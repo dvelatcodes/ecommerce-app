d3587cda28b9cdb412982b958ac85fc205497c0f
Love Olubummo <olubummolove3@gmail.com>
2023-03-27 16:38:02 +0100

    feature: added products and bannerData props to HeroBanner and Product custom tags

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,15 @@ import { client } from "../lib/client";
 const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best selling product</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
       <FooterBanner />
     </>
